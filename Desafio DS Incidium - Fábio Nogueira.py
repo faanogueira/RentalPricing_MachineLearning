f412e3662789b20099ba77efe185da3049fc73ef
Fábio Nogueira <150953726+faanogueira@users.noreply.github.com>
2025-01-31 23:10:32 -0300

    upload inicial

---
+++ Desafio DS Incidium - Fábio Nogueira.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+
+## Alterando a exibição de DataFrames do Pandas
+
+
+# In[3]:
+
+
+pd.set_option('display.max_columns', None)
+
+
+# In[5]:
+
+
+df = pd.read_csv('assets/teste_indicium_precificacao.csv', sep=";", encoding='cp1252')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
